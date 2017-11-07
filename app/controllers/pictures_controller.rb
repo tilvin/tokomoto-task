436d8316d920da8863095842cb4b66cbdd5e13fd
Tilvin <kate2homy@gmail.com>
2017-11-07 19:37:42 +0300

    fix file verification on pictures index page

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -2,8 +2,8 @@ class PicturesController < ApplicationController
 
   def index
     @image_hash = Dir["app/assets/images/*"]
-      .map { |f| [File.basename(f), File.size(f)] }
-      .group_by{ |filename, size| size }
+      .map { |f| [File.basename(f), Digest::MD5.digest(File.read(f))] }
+      .group_by{ |filename, md5| md5 }
       .map { |k, v| { filename: v.first.first, count: v.count } }
       .sort_by { |data| - data[:count] }
   end
